1da48a23dd18f520e3796d5790911a0853ddc574
RiazK <RiazCKhan@gmail.com>
2022-01-11 20:32:38 -0500

    implement artivle title validation and render load if false

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,7 @@ class ArticlesController < ApplicationController
 
   #new
   def new
+    @article = Article.new
   end
 
   #edit
@@ -19,8 +20,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
    
-    @article.save
+    if @article.save
     redirect_to @article
+    else
+      render 'new'
+    end
   end
 
   private
