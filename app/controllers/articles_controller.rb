9cc0ec1895b587691d59fc2137276b915fb16de7
RiazK <RiazCKhan@gmail.com>
2022-01-11 20:21:22 -0500

    Create and Show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,36 @@
 class ArticlesController < ApplicationController
+  #index
+  def index
+    @articles = Article.all
+  end
+
+  #show
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  #new
   def new
   end
+
+  #edit
+
+  #create
+  def create
+    @article = Article.new(article_params)
+   
+    @article.save
+    redirect_to @article
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+  #update
+
+  #destory
+
+
 end
